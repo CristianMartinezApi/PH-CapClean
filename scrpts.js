ca5723ea002a9a501fe8ef8c206fd671232cebc7
CristianMartinezApi <fernandesribe04@gmail.com>
2025-02-28 10:57:27 -0300

    ajuste whatsapp

--- scrpts.js
+++ scrpts.js
@@ -31,7 +31,7 @@ function enviarWatsapp(event) {
 
     const nome = document.getElementById("nome").value;
     const mensagem = document.getElementById("mensagem").value;
-    const telefone = "5548998354402";
+    const telefone = "5548999857463";
     const texto = `Olá, me chamo ${nome} e gostaria de falar sobre: ${mensagem}`;
     const msgFormatada = encodeURIComponent(texto);
     const url = `https://api.whatsapp.com/send?phone=${telefone}&text=${msgFormatada}`;
