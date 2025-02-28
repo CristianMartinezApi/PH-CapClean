5d3e2e2785b7c8c87d5ea4d1b2c9daba2efdb19c
CristianMartinezApi <fernandesribe04@gmail.com>
2025-02-28 10:51:05 -0300

    whatsapp

--- scrpts.js
+++ scrpts.js
@@ -25,3 +25,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     updateItem(currentIndex);
 });
+/* whatssap */
+function enviarWatsapp(event) {
+    event.preventDefault();
+
+    const nome = document.getElementById("nome").value;
+    const mensagem = document.getElementById("mensagem").value;
+    const telefone = "5548998354402";
+    const texto = `Olá, me chamo ${nome} e gostaria de falar sobre: ${mensagem}`;
+    const msgFormatada = encodeURIComponent(texto);
+    const url = `https://api.whatsapp.com/send?phone=${telefone}&text=${msgFormatada}`;
+    window.open(url, "_blank");
+  }
